1d65dd075fbb5a551bb4c2e4a7225414bf107905
anwarismail <anwaarismail449@gmail.com>
2023-06-22 17:03:22 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function fetchBeerOne(){
+    fetch(`http://localhost:3000/beers/${1}`)
+    .then(res=>res.json())
+    .then(data=>displayBeerOne(data))
+    }
+    fetchBeerOne()
+    
+    function displayBeerOne(beer0ne){
+        let h1=document.getElementById("beer-description")
+        h1.textContent=`${beer0ne.description}`
+        let name=document.getElementById("beer-name")
+        name.textContent=`${beer0ne.name}`
+        let ul=document.getElementById("review-list")
+        ul.innerHTML = '';
+        beer0ne.reviews.forEach(review=>{
+            const li = document.createElement('li');
+            li.textContent = review;
+            ul.appendChild(li);
+        })
+       
+    }
